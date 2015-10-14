792198593c5cb3fbe116864093337bdf3276b975
Mahmudul Hasan <mhasa004@ucr.edu>
2015-10-14 16:42:45 -0700

    added unpooling layer without test

--- src/caffe/layer_factory.cpp
+++ src/caffe/layer_factory.cpp
@@ -70,6 +70,22 @@ shared_ptr<Layer<Dtype> > GetPoolingLayer(const LayerParameter& param) {
 
 REGISTER_LAYER_CREATOR(Pooling, GetPoolingLayer);
 
+// Get unpooling layer according to engine.
+template <typename Dtype>
+shared_ptr<Layer<Dtype> > GetUnpoolingLayer(const LayerParameter& param) {
+  UnpoolingParameter_Engine engine = param.unpooling_param().engine();
+  if (engine == UnpoolingParameter_Engine_DEFAULT) {
+    engine = UnpoolingParameter_Engine_CAFFE;
+  }
+  if (engine == UnpoolingParameter_Engine_CAFFE) {
+    return shared_ptr<Layer<Dtype> >(new UnpoolingLayer<Dtype>(param));
+  } else {
+    LOG(FATAL) << "Layer " << param.name() << " has unknown engine.";
+  }
+}
+
+REGISTER_LAYER_CREATOR(Unpooling, GetUnpoolingLayer);
+
 // Get relu layer according to engine.
 template <typename Dtype>
 shared_ptr<Layer<Dtype> > GetReLULayer(const LayerParameter& param) {
